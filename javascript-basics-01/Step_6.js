62e9edd6c181b5c14c18c73ad3f1ad895d9f36a5
fatima-katrib <fatimakatrib19@gnail.com>
2022-12-07 16:38:45 +0200

    edited Step_6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,3 +1,4 @@
 const nb_1 =prompt("Enter the first number?"), nb_2 =prompt("Enter the second number?");
 let division = nb_1/nb_2;
-alert(`result of ${nb_1}*${nb_2} = ${division}`); 
+let remainder=nb_1%nb_2;
+alert(`the reaminder of ${nb_1}/${nb_2} = ${remainder}`); 
